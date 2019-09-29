4ff03afcf097722ce4c3a2c50e92d5398c4ba670
chenxz <563859095@qq.com>
2019-09-29 11:16:15 +0800

    commit

--- app/src/main/java/com/cxz/bigview/sample/MainActivity.java
+++ app/src/main/java/com/cxz/bigview/sample/MainActivity.java
@@ -4,12 +4,27 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cxz.bigviewlib.BigView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BigView bigView = findViewById(R.id.bigView);
+        InputStream is = null;
+        try {
+            is = getResources().getAssets().open("big.jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        bigView.setImage(is);
+
     }
 
 }
